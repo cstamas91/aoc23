b96ca1b9c4636749900b728c0b831c0f841112cf
cstamas91 <cstamas91@users.noreply.github.com>
2023-12-02 15:25:27 +0000

    solved 1

--- 1/Program.cs
+++ 1/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var inputLines = File.ReadAllText("input.txt").Split(Environment.NewLine);
+
+static int GetCalibrationValueFromLine(ReadOnlySpan<char> line) 
+{
+    var leftIndex = 0;
+    var rightIndex = line.Length - 1;
+    int? leftVal = null;
+    int? rightVal = null;
+
+    while (leftVal is null && leftIndex < line.Length)
+    {
+        if (int.TryParse(line[leftIndex].ToString(), out int number))
+        {
+            leftVal = number;
+        }
+
+        leftIndex++;
+    }
+
+    while (rightVal is null && rightIndex >= 0)
+    {
+        if (int.TryParse(line[rightIndex].ToString(), out int number))
+        {
+            rightVal = number;
+        }
+
+        rightIndex--;
+    }    
+
+    return ((leftVal ?? 0) * 10) + (rightVal ?? 0);
+}
+
+Console.WriteLine(inputLines.Select(l => GetCalibrationValueFromLine(l)).Sum());
+
