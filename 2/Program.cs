5283f722da9b691d4b690855d2596237975cf5a3
cstamas91 <cstamas91@users.noreply.github.com>
2023-12-02 16:27:37 +0000

    task 2 done

--- 2/Program.cs
+++ 2/Program.cs
@@ -1,34 +1,69 @@
-﻿var inputLines = File.ReadAllText("input.txt").Split(Environment.NewLine);
+﻿using System.Text;
 
-static int GetCalibrationValueFromLine(ReadOnlySpan<char> line) 
+var examples = File.ReadAllText("exampleInput.txt").Split(Environment.NewLine);
+var inputLines = File.ReadAllText("input.txt").Split(Environment.NewLine);
+
+var wordToInt = new Dictionary<string, int>()
 {
-    var leftIndex = 0;
-    var rightIndex = line.Length - 1;
-    int? leftVal = null;
-    int? rightVal = null;
+    {"one", 1},
+    {"two", 2},
+    {"three", 3},
+    {"four", 4},
+    {"five", 5},
+    {"six", 6},
+    {"seven", 7},
+    {"eight", 8},
+    {"nine", 9},
+    {"1", 1},
+    {"2", 2},
+    {"3", 3},
+    {"4", 4},
+    {"5", 5},
+    {"6", 6},
+    {"7", 7},
+    {"8", 8},
+    {"9", 9},
+    {"0", 0},
+};
 
-    while (leftVal is null && leftIndex < line.Length)
+static string DebugDict(Dictionary<int, int> dict) 
+{
+    var sb = new StringBuilder();
+    foreach (var k in dict.Keys)
     {
-        if (int.TryParse(line[leftIndex].ToString(), out int number))
-        {
-            leftVal = number;
-        }
-
-        leftIndex++;
+        sb.Append($"{k}: {dict[k]},");
     }
 
-    while (rightVal is null && rightIndex >= 0)
-    {
-        if (int.TryParse(line[rightIndex].ToString(), out int number))
-        {
-            rightVal = number;
-        }
-
-        rightIndex--;
-    }    
-
-    return ((leftVal ?? 0) * 10) + (rightVal ?? 0);
+    return sb.ToString();
 }
 
-Console.WriteLine(inputLines.Select(l => GetCalibrationValueFromLine(l)).Sum());
+static int GetCalibrationValueFromLine(Dictionary<string, int> words, ReadOnlySpan<char> line) 
+{
+    var matches = new Dictionary<int, int>();
+    foreach (var key in words.Keys)
+    {
+        var index = line.IndexOf(key);
+        if (index > -1)
+        {
+            matches.Add(index, words[key]);
+        }
 
+        var lastIndex = line.LastIndexOf(key);
+        if (lastIndex > -1 && lastIndex != index)
+        {
+            matches.Add(lastIndex, words[key]);
+        }
+    }
+
+    var minKey = matches.Keys.Min();
+    var maxKey = matches.Keys.Max();
+
+    var res = (matches[minKey] * 10) + matches[maxKey];
+
+    return res;
+}
+
+// Console.WriteLine("example");
+// Console.WriteLine(examples.Select(l => GetCalibrationValueFromLine(wordToInt, l)).Sum());
+Console.WriteLine("inputs");
+Console.WriteLine(inputLines.Select(l => GetCalibrationValueFromLine(wordToInt, l)).Sum());
