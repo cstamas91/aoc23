e73f017cd40653b5b04e1b5c63e54d8bd9255b8d
cstamas91 <cstamas91@users.noreply.github.com>
2023-12-02 15:26:30 +0000

    project for task 2

---
+++ 2/Program.cs
@@ -0,0 +1,34 @@
+﻿var inputLines = File.ReadAllText("input.txt").Split(Environment.NewLine);
+
+static int GetCalibrationValueFromLine(ReadOnlySpan<char> line) 
+{
+    var leftIndex = 0;
+    var rightIndex = line.Length - 1;
+    int? leftVal = null;
+    int? rightVal = null;
+
+    while (leftVal is null && leftIndex < line.Length)
+    {
+        if (int.TryParse(line[leftIndex].ToString(), out int number))
+        {
+            leftVal = number;
+        }
+
+        leftIndex++;
+    }
+
+    while (rightVal is null && rightIndex >= 0)
+    {
+        if (int.TryParse(line[rightIndex].ToString(), out int number))
+        {
+            rightVal = number;
+        }
+
+        rightIndex--;
+    }    
+
+    return ((leftVal ?? 0) * 10) + (rightVal ?? 0);
+}
+
+Console.WriteLine(inputLines.Select(l => GetCalibrationValueFromLine(l)).Sum());
+
