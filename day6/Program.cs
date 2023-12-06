a71373d3f1806740767639483f81df5958ebb3fc
cstamas91 <cstamas91@users.noreply.github.com>
2023-12-06 10:33:41 +0000

    part1 done

--- day6/Program.cs
+++ day6/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var input = File.ReadAllText(args[0])
+    .Split(Environment.NewLine);
+
+var times = input[0].Replace("Time:", "").Trim().Split(" ").Select(s => s.Trim()).Where(s => !string.IsNullOrEmpty(s)).Select(int.Parse).ToList();
+var records = input[1].Replace("Distance:", "").Trim().Split(" ").Select(s => s.Trim()).Where(s => !string.IsNullOrEmpty(s)).Select(int.Parse).ToList();
+var races = new List<Race>();
+for (int i = 0; i < times.Count; i++)
+{
+    races.Add(new Race(times[i], records[i]));
+}
+
+Console.WriteLine(races.Aggregate(1, (curr, nxt) => curr * nxt.GetRecordBeatingInputCount()));
+
+class Race
+{
+    private int Length {get;init;}
+    private int RecordToBeat {get;init;}
+
+    public Race(int length, int recordToBeat)
+    {
+        Length = length;
+        RecordToBeat = recordToBeat;
+    }
+
+    public int GetRecordBeatingInputCount()
+    {
+        return Enumerable.Range(0, Length).Where(i => i * (Length - i) > RecordToBeat).Count();
+    }
+}
