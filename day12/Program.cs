e093afc1aafa91f27a4f9e85d17b11143a2eb93f
cstamas91 <cstamas91@users.noreply.github.com>
2023-12-18 08:33:57 +0000

    day12 inprogress

--- day12/Program.cs
+++ day12/Program.cs
@@ -1,2 +1,86 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text;
+using System.Text.RegularExpressions;
+
+var input = File.ReadAllText("example").Split(Environment.NewLine);
+
+var records = input.Select(l => new Record(l)).ToList();
+
+foreach (var record in records)
+{
+    Console.WriteLine($"{record.conditionPart} -> {record.GetPossiblePermutationCount()}");
+}
+
+Console.WriteLine(records.Sum(r => r.GetPossiblePermutationCount()));
+
+class Record
+{
+    private const char WORKING_SPRING = '.';
+    private const char BROKEN_SPRING = '#';
+    private const char UNKNOWN_SPRING = '?';
+    public readonly string conditionPart;
+    private readonly List<int> revisedRecord;
+    private readonly Regex revisionRegex;
+
+    public Record(string input)
+    {
+        var parts = input.Split(' ');
+        conditionPart = parts[0];
+        revisedRecord = parts[1].Split(',').Select(int.Parse).ToList();
+        revisionRegex = BuildRegex();
+    }
+
+    private bool IsRevisedRecordSatisfied(string conditionRecord)
+    {
+        return revisionRegex.IsMatch(conditionRecord);
+    }
+
+    private Regex BuildRegex()
+    {
+        var sb = new StringBuilder();
+        sb.Append(@"(^|\.+)");
+        sb.Append(string.Join(@"[\.\?]+", revisedRecord.Select(i => $@"[#\?]{{{i}}}")));
+        sb.Append(@"($|\.+)");
+
+        return new Regex(sb.ToString());
+    }
+
+    private List<string> GetPermutations()
+    {
+        var result = new List<string>(){string.Empty};
+
+        for (int i = 0; i < conditionPart.Length; i++)
+        {
+            result = AddChar(result, conditionPart[i]);
+        }
+
+        return result;
+    }
+
+    private static List<string> AddChar(List<string> currentPermutations, char c)
+    {
+        var result = new List<string>();
+        if (c == WORKING_SPRING || c == BROKEN_SPRING)
+        {
+            foreach (var p in currentPermutations)
+            {
+                result.Add(p + c);
+            }
+
+            return result;
+        }
+
+        foreach (var perm in currentPermutations)
+        {
+            result.Add(perm + WORKING_SPRING);
+            result.Add(perm + BROKEN_SPRING);
+        }
+
+        return result;
+    }
+
+    public int GetPossiblePermutationCount()
+    {
+        var permutations = GetPermutations();
+        return permutations.Count(IsRevisedRecordSatisfied);
+    }
+}
